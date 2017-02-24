eacc40a25f3256683fe118424f86c2fabc101535
ibolight91 <ibolight91@gmail.com>
2017-02-24 13:07:03 +0100

    navigation bar added

--- app/src/main/java/com/example/ibrahim/unibook/BookSingleActivity.java
+++ app/src/main/java/com/example/ibrahim/unibook/BookSingleActivity.java
@@ -1,7 +1,11 @@
 package com.example.ibrahim.unibook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ import com.squareup.picasso.Picasso;
 public class BookSingleActivity extends AppCompatActivity {
 
     private String mPost_key=null;
+    private BottomNavigationView bottomNavigationView;
     private DatabaseReference mDatabase;
     private ImageView mBookSingleImage;
     private TextView mBookSingleTitle, mBookSingleDesc, mBookSinglePrice;
@@ -28,6 +33,23 @@ public class BookSingleActivity extends AppCompatActivity {
         mBookSingleDesc = (TextView) findViewById(R.id.descField);
         mBookSinglePrice = (TextView) findViewById(R.id.priceField);
         mBookSingleImage=(ImageView)findViewById(R.id.singleBookImage);
+
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.action_home){
+                    startActivity(new Intent(BookSingleActivity.this, MainActivity.class));
+                }
+                else if(item.getItemId()==R.id.action_chat){
+                    startActivity(new Intent(BookSingleActivity.this, ChatActivity.class));
+                }
+                else if(item.getItemId()==R.id.action_map){
+                    startActivity(new Intent(BookSingleActivity.this, MapsActivity.class));
+                }
+                return false;
+            }
+        });
 
         mDatabase.child(mPost_key).addValueEventListener(new ValueEventListener() {
             @Override
